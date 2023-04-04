515c0687a7942807e88f8ecdd4f8a8f4529b2019
ewaldvandyk <ewald.redmage@gmail.com>
2023-04-04 12:50:49 +0200

    Slight change in timepoint pattern

--- gen_ref_tree.R
+++ gen_ref_tree.R
@@ -1,13 +1,13 @@
 #Generate a new reference tree for gating strategy for a fixed cohort
 
 #Input parameters
-cohort_folder <- "/Volumes/Groups/GroupDeVisser/data/Hannah/Human blood/BELLINI" # Data folder containing samples
+cohort_folder <- "~/data/Noor/flowJow_2020_09_18/" # Data folder containing samples
 output_refTree_file <- "~/devel/R/flowJo_merge/output/2021_10_15/ref_tree_v1.yaml" #Text file with reference tree and aliases
 
 #Data search parameters
 file_pattern <- "FlowJo.*\\.xls$"
-time_pattern <- "1\\s*([sS][tT][eE]|[dD][eE]|[sS][tT]|[nN][dD]|[rR][dD]|[tT][hH])?\\s*timepoint\\s*$"
-# time_pattern <- NULL # Comment out if you want to use timepoint subfolders
+# time_pattern <- "1\\s*([sS][tT][eE]|[dD][eE]|[sS][tT]|[nN][dD]|[rR][dD]|[tT][hH])?\\s*timepoint\\s*$"
+time_pattern <- NULL # Comment out if you want to use timepoint subfolders
 
 #Get current file location
 srcFile <- NULL
